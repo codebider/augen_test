5b41ce85a6da58595d4c3bd21203c31137db6a45
DamLd <dam@pretty.tips>
2019-01-13 15:48:07 +0700

    add database config + migrate

---
+++ augen_api/config/db.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "development": {
+        "username": null,
+        "password": null,
+        "database": "nodejs_dev",
+        "host": "localhost",
+        "dialect": "postgresql"
+    },
+    "test": {
+        "username": null,
+        "password": null,
+        "logging": false,
+        "database": "nodejs_test",
+        "host": "localhost",
+        "dialect": "postgresql"
+    },
+    "production": {
+        "username": "daniel",
+        "password": "p@ssword",
+        "database": "nodejs_prod",
+        "host": "localhost",
+        "dialect": "postgresql"
+    }
+}
